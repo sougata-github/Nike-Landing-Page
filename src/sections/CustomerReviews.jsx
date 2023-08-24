5e35fb47371588292babf8f139106c97096c362a
SOUGATA DAS <crusaderbeastincarnate@gmail.com>
2023-08-25 02:04:26 +0530

    change animation styles of customerReview.jsx.

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -14,7 +14,7 @@ const CustomerReviews = () => {
         exceptional experiences with us.
       </p>
 
-      <div className='mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14'  data-aos="fade-down">
+      <div className='mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14'  data-aos="fade-up">
         {reviews.map((review, index) => (
           <ReviewCard
             key={index}
