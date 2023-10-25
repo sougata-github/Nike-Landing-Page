8931666c7f740f8121ea569081362e96d4233968
Sougata <sougatadas9874@gmail.com>
2023-10-25 14:10:56 +0530

    add close icon to mobile nav.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,6 +2,7 @@ import { hamburger } from "../assets/icons";
 import { headerLogo } from "../assets/images";
 import { navLinks } from "../constants";
 import { useState } from "react";
+import close from "../assets/icons/close.png";
 
 const Nav = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
@@ -32,38 +33,36 @@ const Nav = () => {
           ))}
         </ul>
 
-        {toggleMenu ? (
-          <div className=" ease-in-out duration-100 pt-4 pb-8 px-10 absolute top-[20px] right-[10px] hidden max-lg:flex flex-col items-end text-right rounded-lg bg-black z-20">
-            <button
-              className="my-2 mr-2 font-semibold text-xl leading-none pb-5 text-white"
-              onClick={() => {
-                setToggleMenu(false);
-              }}
-            >
-              x
-            </button>
-            <ul className="flex justify-center items-center flex-col gap-12 py-2 text-lg">
+        <div className="hidden max-lg:block transition-all ease-in-out cursor-pointer duration-100">
+          <img
+            src={toggleMenu ? close : hamburger}
+            alt="hamburger icon"
+            width={22}
+            height={22}
+            onClick={() => setToggleMenu((prev) => !prev)}
+          />
+        </div>
+        {toggleMenu && (
+          <div
+            className=" ease-in-out duration-100 pt-4 pb-8 px-10 transition-all
+            absolute top-[75px] right-[40px] 
+            hidden max-lg:flex 
+            flex-col items-end 
+            text-right 
+            rounded-md bg-gray-100 z-20"
+          >
+            <ul className="flex justify-center items-center flex-col gap-12 py-2 text-lg ">
               {navLinks.map((item) => (
                 <li key={item.label}>
                   <a
                     href={item.href}
-                    className="font-montserrat leading-normal text-lg text-white"
+                    className="font-montserrat leading-normal text-lg text-gray-500 hover:text-black"
                   >
                     {item.label}
                   </a>
                 </li>
               ))}
             </ul>
-          </div>
-        ) : (
-          <div className="hidden max-lg:block">
-            <img
-              src={hamburger}
-              alt="hamburger icon"
-              width={25}
-              height={25}
-              onClick={() => setToggleMenu(true)}
-            />
           </div>
         )}
       </nav>
